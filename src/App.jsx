de2639bee3fd3d806f3c402f3bb10a75be0f2a2d
ammar <ammar.botonjic@convergent-usa.com>
2021-02-07 15:45:56 +0100

    Rendering map, calculations for y and x - generating squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Container,  BoardGrid, Square } from './2DArray.styled';
+import { shipArray } from './renderCoordinates.helpers';
 
 const App = () => {
+  const [mapa, setMapa] = useState(new Array(100).fill(0));
 
-  const mapa = new Array(100).fill(0);
+  const fillMapWithCoordinates = () => {
+    shipArray.forEach((ship) => ship.coordinates.forEach(coordIndex => {
+      mapa[coordIndex] = ship.class
+    }))
+  }
 
   return (
     <Container>
       <h1>Battlefield Game</h1>
       <div className="grid-display">
-      <BoardGrid size="600px">
-        { mapa.map(kocka => <Square>{kocka}</Square>) }
-      </BoardGrid>
+        <BoardGrid size="600px">
+          {fillMapWithCoordinates()}
+          { mapa.map((color, index) => <Square style={{ backgroundColor: color }} key={index} /> ) }
+        </BoardGrid>
       </div>
     </Container>
   )
---
+++ src/renderCoordinates.helpers.js
@@ -0,0 +1,69 @@
+//ships
+export const shipArray = [
+    {
+      name: 'destroyer',
+      size: 2,
+      class: 'red',
+      coordinates: []
+    },
+    {
+      name: 'submarine',
+      size: 3,
+      class: 'green',
+      coordinates: []
+    },
+    {
+      name: 'cruiser',
+      size: 3,
+      class: 'green',
+      coordinates: []
+    },
+    {
+      name: 'battleship',
+      size: 4,
+      class: 'purple',
+      coordinates: []
+    },
+    {
+      name: 'carrier',
+      size: 5,
+      class: 'blue',
+      coordinates: []
+    }
+]
+
+const getDirection = (pocetna, size) => {
+  // get direction - 0 vertical, 1 horizontal
+  let direction = Math.floor(Math.random())
+
+  const y = Math.floor(pocetna / 10)
+  const x = pocetna - (y * 10)
+
+  if (direction === 0) {
+    if (y - size <= 0) direction = 10
+    if (y + size >= 10) direction = -10
+    else direction = 10
+  }
+  if (direction === 1) {
+    if (x - size <= 0) direction = 1
+    if (x + size >= 10) direction = -1
+    else direction = 1
+  }
+
+  return direction;
+}
+
+const fillShipsCoordinates = () => {
+  shipArray.map((ship) => {
+    let pocetnaRandomPozicija = Math.floor(Math.random() * 100);
+    const smjer = getDirection(pocetnaRandomPozicija, ship.size);
+
+    for (let index = 0; index < ship.size; index++) {
+      if (ship.coordinates.includes(smjer)) pocetnaRandomPozicija = Math.floor(Math.random() * 100);
+      ship.coordinates.push(pocetnaRandomPozicija)
+      pocetnaRandomPozicija += smjer
+    }
+  })
+}
+
+fillShipsCoordinates()
