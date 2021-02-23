cabbc9f932534e5a98f6767d55997c692b2844c6
ammar <ammar.botonjic@convergent-usa.com>
2021-02-23 14:21:07 +0100

    Collision check, rendering ships on map

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import { Container,  BoardGrid, Square } from './2DArray.styled';
 import { shipArray } from './renderCoordinates.helpers';
+import { size } from './constants/constants';
 
 const App = () => {
-  const [mapa, setMapa] = useState(new Array(100).fill(0));
+  const [mapa, setMapa] = useState(new Array(size * size).fill(0));
 
   const fillMapWithCoordinates = () => {
     shipArray.forEach((ship) => ship.coordinates.forEach(coordIndex => {
       mapa[coordIndex] = ship.class
     }))
   }
-
   return (
     <Container>
       <h1>Battlefield Game</h1>
--- src/renderCoordinates.helpers.js
+++ src/renderCoordinates.helpers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 //ships
 export const shipArray = [
     {
@@ -5,63 +6,92 @@ export const shipArray = [
       size: 2,
       class: 'red',
       coordinates: []
-    },
+    }, 
     {
       name: 'submarine',
       size: 3,
-      class: 'green',
+      class: 'purple',
       coordinates: []
     },
     {
       name: 'cruiser',
       size: 3,
-      class: 'green',
+      class: 'purple',
       coordinates: []
     },
     {
       name: 'battleship',
       size: 4,
-      class: 'purple',
+      class: 'green',
       coordinates: []
     },
     {
       name: 'carrier',
       size: 5,
-      class: 'blue',
+      class: 'yellow',
       coordinates: []
-    }
+    },
 ]
-
 const getDirection = (pocetna, size) => {
   // get direction - 0 vertical, 1 horizontal
-  let direction = Math.floor(Math.random())
+  let direction = Math.floor(Math.random() * 2)
 
   const y = Math.floor(pocetna / 10)
   const x = pocetna - (y * 10)
 
   if (direction === 0) {
     if (y - size <= 0) direction = 10
-    if (y + size >= 10) direction = -10
+    else if (y + size >= 10) direction = -10
     else direction = 10
   }
   if (direction === 1) {
     if (x - size <= 0) direction = 1
-    if (x + size >= 10) direction = -1
+    else if (x + size >= 10) direction = -1
     else direction = 1
   }
 
   return direction;
 }
 
-const fillShipsCoordinates = () => {
-  shipArray.map((ship) => {
-    let pocetnaRandomPozicija = Math.floor(Math.random() * 100);
-    const smjer = getDirection(pocetnaRandomPozicija, ship.size);
 
-    for (let index = 0; index < ship.size; index++) {
-      ship.coordinates.push(pocetnaRandomPozicija)
-      pocetnaRandomPozicija += smjer
+const isColision = (coordinate) => {
+  let result = false;
+  for (let i = 0; i < shipArray.length; i++) {
+    const ship = shipArray[i];
+
+    if (ship.coordinates.includes(coordinate) || ship.coordinates.includes(coordinate + 1) || ship.coordinates.includes(coordinate - 1) || ship.coordinates.includes(coordinate + 10) || ship.coordinates.includes(coordinate - 10)) {
+      result = true;
+      break;
     }
+  }
+  return result;
+}
+
+
+const fillShipsCoordinates = () => {
+  shipArray.forEach((ship) => {
+    let tempCoordinates = [];
+    let doWork = true;
+
+    while (doWork) {
+      let coordinate = Math.floor(Math.random() * 100);
+      let direction = getDirection(coordinate, ship.size);
+
+      for (let i = 0; i < ship.size; i++) {
+        if (isColision(coordinate)) {
+          tempCoordinates = [];
+          break;
+        }
+
+        tempCoordinates.push(coordinate);
+        coordinate += direction;
+
+        if (tempCoordinates.length === ship.size) {
+          doWork = false;
+        }
+      }
+    }
+    ship.coordinates.push(...tempCoordinates);
   })
 }
 
