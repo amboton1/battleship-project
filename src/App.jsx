c4676616576ebdf2ce4a4c59bce45276860d2a21
Ammar Botonjić <botonjic.ab@pg.com>
2021-02-28 13:44:16 +0100

    Moving ships array to constants, setting final mapa state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Container,  BoardGrid, Square } from './2DArray.styled';
-import { shipArray } from './renderCoordinates.helpers';
-import { size } from './constants/constants';
+import { fillShipsCoordinates } from './renderCoordinates.helpers';
+import { shipArray, size } from './constants/constants';
 
 const App = () => {
-  const [mapa, setMapa] = useState(new Array(size * size).fill(0));
+  const [mapa, setMapa] = useState(new Array(size * size).fill(null));
+  const tempArray = new Array(size * size).fill(null);
 
-  const fillMapWithCoordinates = () => {
-    shipArray.forEach((ship) => ship.coordinates.forEach(coordIndex => {
-      mapa[coordIndex] = ship.class
+  useEffect(() => {
+    fillShipsCoordinates(shipArray);
+  }, [])
+
+  useEffect(() => {
+    shipArray.forEach((ship) => ship.coordinates.forEach(coordinates => {
+      tempArray[coordinates] = ship.class;
     }))
-  }
+
+    setMapa(tempArray);
+  }, [])
+
   return (
     <Container>
       <h1>Battlefield Game</h1>
       <div className="grid-display">
         <BoardGrid size="600px">
-          {fillMapWithCoordinates()}
           { mapa.map((color, index) => <Square style={{ backgroundColor: color }} key={index} /> ) }
         </BoardGrid>
       </div>
